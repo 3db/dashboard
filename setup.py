e311e9d323094f886df7b8a84d67d46c637e12e8
Guillaume Leclerc <leclerc@mit.edu>
2021-06-07 02:21:57 -0400

    Fix missing folder in pypi package

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class BuildDashboardCommand(distutils.cmd.Command):
 
 
 setup(name='threedboard',
-      version='0.0.4',
+      version='0.0.5',
       description='Web Interface to visualize the results of 3DB experiments',
       url='https://github.com/3db/dashboard',
       author='3DB authors',
